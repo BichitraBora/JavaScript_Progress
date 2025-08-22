b497002c5f3edd9cd231797236b60f699a987e88
Bichitra <bichitrabora500@gmail.com>
2025-08-22 06:33:15 +0530

    Conversion of Datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = "Vishu"
+console.log(typeof score)
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "Vishu"
+let isLoggedInBoolean = Boolean(isLoggedIn)
+console.log(isLoggedInBoolean);
+
+// 1 => true; 0 => false
+// "Vishu" => true; "" => false
+
+let someNumber = 25
+let someNumberString = String(someNumber)
+console.log(someNumberString);
+console.log(typeof someNumberString);
