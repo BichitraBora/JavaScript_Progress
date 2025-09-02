93060f878018cf94e6e09720754786e2df8459bb
Bichitra <bichitrabora500@gmail.com>
2025-09-02 23:08:16 +0530

    Stack & Heap

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "Vishu"
-console.log(typeof score)
-console.log(typeof (score));
+// console.log(typeof score)
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -12,12 +12,51 @@ console.log(valueInNumber);
 
 let isLoggedIn = "Vishu"
 let isLoggedInBoolean = Boolean(isLoggedIn)
-console.log(isLoggedInBoolean);
+// console.log(isLoggedInBoolean);
 
 // 1 => true; 0 => false
 // "Vishu" => true; "" => false
 
 let someNumber = 25
 let someNumberString = String(someNumber)
-console.log(someNumberString);
-console.log(typeof someNumberString);
+// console.log(someNumberString);
+// console.log(typeof someNumberString);
+
+// **************** Operations *****************
+let value = 10
+let negValue = -value
+// console.log(negValue);
+
+// console.log(5 + 5); // addition operator
+// console.log(5 - 5); // subtraction operator
+// console.log(5 * 5); // multiplication operator
+// console.log(5 ** 5); // exponentiation operator
+// console.log(5 / 5); // division operator
+// console.log(5 % 5); // modulus operator
+
+
+ let str1 = "Hello"
+ let str2 = " Vishu"
+//  console.log(str1 + str2); // concatenation operator
+
+//  console.log(1 + "5");
+//  console.log("1"+ 5);
+//  console.log(1 + 2 + "5");
+//  console.log("1" + 2 + 5);
+//  console.log( (3 + 4)* 5 % 3);
+ 
+ 
+// Not preferred for production purposes
+ console.log(+true);
+ console.log(+"");
+ console.log(true);
+
+//  let num1, num2, num3
+//  num1 = num2 = num3 = 5 + 5
+
+
+ let gameCounter = 100
+ ++gameCounter;
+ console.log(gameCounter);
+ 
+ 
---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!= 1);
+
+// console.log("2" == 2);
+// console.log("2" <= 2);
+// console.log("2" >= 2);        { in most cases we don't use this type of comparisons } 
+// console.log("2" < 2);
+// console.log("2" > 2);
+// console.log("2" + 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+// console.log(undefined != 0);
+
+// ************** Strict Check ****************
+console.log("2" === 2); // False because of strick check
